db7bfa27b47057472e6d4cf1eeb68b5ed9297cfd
BaidLavanya <75931821+BaidLavanya@users.noreply.github.com>
2020-12-13 15:57:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function setDate() {
+    document.getElementById("date").innerHTML = new Date().toDateString();
+}
+
+function changecolor(){
+    document.getElementById("heading").style.color="black";
+    
+}
